e2627860220d9e843a9ac42b1478374e1b07c125
Sungin Jung <1986seoul@gmail.com>
2018-02-25 23:20:53 -0600

    Add project files.

---
+++ WebScraper/Data/ScrapeCriteriaPart.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace WebScraper.Data
+{
+    class ScrapeCriteriaPart
+    {
+        public string Regex { get; set; }
+        public RegexOptions RegexOption { get; set; }
+
+    }
+}
---
+++ WebScraper/Program.cs
@@ -0,0 +1,67 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using WebScraper.Builders;
+using WebScraper.Data;
+using WebScraper.Workers;
+
+namespace WebScraper
+{
+    class Program
+    {
+        private const string Method = "search";
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter City: ");
+                var cityName = Console.ReadLine() ?? string.Empty;
+
+                Console.Write("Enter Category: ");
+                var categoryName = Console.ReadLine() ?? string.Empty;
+
+                using (WebClient client = new WebClient())
+                {
+                    string content = client.DownloadString($"http://{cityName.Replace(" ", string.Empty)}.craigslist.org/{Method}/{categoryName}");
+
+                    ScrapeCriteria scrapeCriteria = new ScrapeCriteriaBuilder()
+                        .WithData(content)
+                        .WithRegex(@"<a href=\""(.*?)\"" data-id=\""(.*?)\"" class=\""result-title hdrlnk\"">(.*?)</a>")
+                        .WithRegexOption(RegexOptions.ExplicitCapture)
+                        .WithPart(new ScrapeCriteriaPartBuilder()
+                            .WithRegex(@">(.*?)</a>")
+                            .WithRegexOption(RegexOptions.Singleline)
+                            .Build())
+                        .WithPart(new ScrapeCriteriaPartBuilder()
+                            .WithRegex(@"href=\""(.*?)\""")
+                            .WithRegexOption(RegexOptions.Singleline)
+                            .Build())
+                        .Build();
+
+                    Scraper scraper = new Scraper();
+
+                    var scrapedElements = scraper.Scrape(scrapeCriteria);
+
+                    if (scrapedElements.Any())
+                    {
+                        foreach (var scrapedElement in scrapedElements) Console.WriteLine(scrapedElement);
+                    }
+                    else
+                    {
+                        Console.WriteLine("No match found");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
